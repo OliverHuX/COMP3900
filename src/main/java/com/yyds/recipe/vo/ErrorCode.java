72bab6dbf94e289f99283e570638b8a8bef8bd79
weiqiang <409655689@qq.com>
2021-06-15 09:33:50 +0800

    feat: add error code for the project

---
+++ src/main/java/com/yyds/recipe/vo/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.yyds.recipe.vo;
+
+public interface ErrorCode {
+
+    // Normal error
+
+    int UNKNOWN_ERROR_CODE = 1;
+    String UNKNOWN_ERROR_MESSAGE = "unknown error";
+
+
+    // 701 - 800 service logic error
+
+    // 600 - 700 user role error
+
+    // 801 - 900 middleware error
+
+    // 901 - 1000
+}
