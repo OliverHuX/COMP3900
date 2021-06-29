3a339f39d8ac101fa692bec984e2f280be044a9c
KeKe23333 <41615627+KeKe23333@users.noreply.github.com>
2021-06-29 23:02:08 +0800

    fix hover and some pages style

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -8,13 +8,13 @@ export default class Footer extends Component {
         <div>
             {/*footer in every pages*/}
             <footer className="footer-basic">
-                <div className="social"><a href="#"><i className="icon ion-social-instagram"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-facebook"></i></a></div>
+                <div className="social"><a href="/#"><i className="icon ion-social-instagram"></i></a><a href="/#"><i className="icon ion-social-snapchat"></i></a><a href="/#"><i className="icon ion-social-twitter"></i></a><a href="/#"><i className="icon ion-social-facebook"></i></a></div>
                 <ul className="list-inline">
-                    <li className="list-inline-item"><a href="#">Home</a></li>
-                    <li className="list-inline-item"><a href="#">Services</a></li>
-                    <li className="list-inline-item"><a href="#">About</a></li>
-                    <li className="list-inline-item"><a href="#">Terms</a></li>
-                    <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    <li className="list-inline-item"><a href="/#">Home</a></li>
+                    <li className="list-inline-item"><a href="/#">Services</a></li>
+                    <li className="list-inline-item"><a href="/#">About</a></li>
+                    <li className="list-inline-item"><a href="/#">Terms</a></li>
+                    <li className="list-inline-item"><a href="/#">Privacy Policy</a></li>
                 </ul>
                 <p className="copyright">China - YYDS © 2021</p>
             </footer>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -13,14 +13,16 @@ export default class Home extends Component {
             
         <div>
             <StyledHeader/>
-            <div className="container" id="search-1" style={{marginTop: '70px',marginBottom: '67px',marginRight: '0px',marginLeft: '1150px',}}>
-                <input type="search" style={{width: '500px',height: '40px',}} />
-                <button className="btn btn-primary" type="button" style={{height: '40px', width: '78.9px', marginTop: '-3px', background: 'rgb(102, 215, 215)'}}>
-                    <i className="fa fa-search" style={{height: '0px', fontSize: '25px'}}>
-                    </i>
-                </button>
-            </div>
+
             <div className="container" id="most-popular">
+                <div className="container" id="search-1" style={{marginTop: '70px',marginBottom: '67px',marginRight: '0px',marginLeft: '50px',}}>
+                    
+                    <button className="btn btn-primary" type="button" style={{height: '40px', width: '78.9px', marginTop: '-7px', background: 'rgb(102, 215, 215)'}}>
+                        <i className="fa fa-search" style={{height: '0px', fontSize: '25px'}}>
+                        </i>
+                    </button>
+                    <input type="search" style={{width: '600px',height: '40px',}} />
+                </div>
                 <div className="row" id="popular">
                     <div className="col-md-12" style={{marginTop: '20px', marginBottom: '39px', marginRight: '4px', marginLeft: '10px'}}>
                         <h1 style={{textAlign: 'center'}}>
