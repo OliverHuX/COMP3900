ee1087f78681083fada4ffe6bdb3f955a80b18ab
KeKe23333 <41615627+KeKe23333@users.noreply.github.com>
2021-06-29 23:15:27 +0800

    little style change

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -17,7 +17,7 @@ export default class Home extends Component {
             <div className="container" id="most-popular">
                 <div className="container" id="search-1" style={{marginTop: '70px',marginBottom: '67px',marginRight: '0px',marginLeft: '50px',}}>
                     
-                    <button className="btn btn-primary" type="button" style={{height: '40px', width: '78.9px', marginTop: '-7px', background: 'rgb(102, 215, 215)'}}>
+                    <button className="btn btn-primary" type="button" style={{height: '40px', width: '100px', marginTop: '-7px', background: 'rgb(102, 215, 215)'}}>
                         <i className="fa fa-search" style={{height: '0px', fontSize: '25px'}}>
                         </i>
                     </button>
