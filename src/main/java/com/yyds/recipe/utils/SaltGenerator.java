a3bb6568cab098d332f6e700022d68fe1b6748e2
Boyu Cai <z5183721@ad.unsw.edu.au>
2021-07-03 15:55:38 +1000

    add the encode jwt method

--- src/main/java/com/yyds/recipe/utils/SaltGenerator.java
+++ src/main/java/com/yyds/recipe/utils/SaltGenerator.java
@@ -2,8 +2,11 @@ package com.yyds.recipe.utils;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SaltGenerator {
     public static String getSalt() {
@@ -16,5 +19,14 @@ public class SaltGenerator {
         byte[] bytes = new byte[length];
         secureRandom.nextBytes(bytes);
         return Base64.encodeBase64String(bytes);
+    }
+
+    public static String base64UrlEncode(String str) throws Exception {
+        return Base64.encodeBase64String(str.getBytes("UTF-8"));
+    }
+
+    public static String HMACSHA256(String s, String secret) throws Exception {
+        Mac sha256 = Mac.getInstance("HmacSHA256");
+        return s + Base64.encodeBase64String(sha256.doFinal(secret.getBytes("UTF-8")));
     }
 }
