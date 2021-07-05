298500364da9c31e7c5abe9e81d56f58f437fbcc
Boyu Cai <z5183721@ad.unsw.edu.au>
2021-07-05 17:52:19 +1000

    add new function to create recipe id

--- src/main/java/com/yyds/recipe/utils/UUIDGenerator.java
+++ src/main/java/com/yyds/recipe/utils/UUIDGenerator.java
@@ -6,4 +6,8 @@ public class UUIDGenerator {
     public static String createUserId() {
         return UUID.randomUUID().toString().replace("-", "");
     }
+
+    public static String createRecipeId() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
 }
