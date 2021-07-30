08355e26e4b1c4db06db67a5f65d62e90d1bbdbd
Boyu Cai <z5183721@ad.unsw.edu.au>
2021-07-30 23:58:58 +1000

    constructor for the comment

--- src/main/java/com/yyds/recipe/model/Comment.java
+++ src/main/java/com/yyds/recipe/model/Comment.java
@@ -1,9 +1,13 @@
 package com.yyds.recipe.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Comment implements Serializable {
     private String recipeId;
