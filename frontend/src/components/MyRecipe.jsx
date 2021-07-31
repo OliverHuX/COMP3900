838e5b7a2ef51691f607d6774098342ff85721ff
OliverHuX <permanent0810@gmail.com>
2021-08-01 04:41:44 +0800

    update

--- frontend/src/components/MyRecipe.jsx
+++ frontend/src/components/MyRecipe.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import RecipeCard from './RecipeCard';
 import { useStyles } from './Style';
@@ -19,15 +13,6 @@ export default function MyRecipe() {
     const url = window.location.href.split('/')
     const recipeId = url[url.length - 1]
     console.log(recipeId)
-
-    const handleEdit = () => {
-        window.location.href = '/home/editrecipe/' + recipeId;
-    }
-
-    const handleView = () => {
-        window.location.href = '/home/recipedetail/' + recipeId;
-    }
-
 
     return (
         <React.Fragment>
