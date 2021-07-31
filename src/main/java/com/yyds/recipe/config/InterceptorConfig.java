ad4a5679eb1c2605befb372878848b037a8968a2
weiqiang <409655689@qq.com>
2021-07-31 11:22:21 +0800

    visitor list

--- src/main/java/com/yyds/recipe/config/InterceptorConfig.java
+++ src/main/java/com/yyds/recipe/config/InterceptorConfig.java
@@ -6,26 +6,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // registry.addInterceptor(loginInterceptor())
-                // .addPathPatterns("/**")
-                // .excludePathPatterns(List.of(
-                //         "/home",
-                //         "/login",
-                //         "/register",
-                //         "/emailVerify/**",
-                //         "swagger/**",
-                //         "/swagger-ui.html",
-                //         "swagger-ui.html",
-                //         "/webjars/**",
-                //         "/swagger-ui.html/*",
-                //         "/swagger-resources",
-                //         "/swagger-resources/**",
-                //         "/test"
-                // ));
+        registry.addInterceptor(loginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(List.of(
+                        "/home",
+                        "/login",
+                        "/visitor/recipe_list",
+                        "/register",
+                        "/emailVerify/**",
+                        "swagger/**",
+                        "/swagger-ui.html",
+                        "swagger-ui.html",
+                        "/webjars/**",
+                        "/swagger-ui.html/*",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/test"
+                ));
     }
 
     @Bean
