ae98360590fecc67ccd349e963c3ba554b584b3b
KeKe23333 <41615627+KeKe23333@users.noreply.github.com>
2021-08-01 22:52:42 +0800

    Japanese update

--- frontend/src/components/JapaneseFood.jsx
+++ frontend/src/components/JapaneseFood.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import FoodList from './FoodList'
 import FetchFunc from './fetchFunc';
+// import LikeHeart from './LikeHeart';
 import SelectInput from '@material-ui/core/Select/SelectInput';
-
 
 
 function getInfo(token,setData) {
@@ -17,7 +17,7 @@ function getInfo(token,setData) {
           data.json().then(res => {
             
             setData(data => [...data, res.recipe_lists])
-          
+            
             // console.log('res content', res);
 
             // console.log('res.recipe_lists  ',res.recipe_lists)
@@ -27,6 +27,8 @@ function getInfo(token,setData) {
       .catch(err => console.error('Caught error: ', err))
 }
 
+
+
 const data1 = [
   { recipePhotos:['/assets/img/recipe1.png','/assets/img/recipe2.png'],isLiked:0,likes:10, title: 'AAA', introduction: 'AAAsimple decoration', timeDuration: '11', rateScore: 2 },
   { recipePhotos: ['/assets/img/recipe2.png','/assets/img/recipe1.png'],isLiked:1,likes:20, title: 'BBB', introduction: 'BBBsimple decoration', timeDuration: '20', rateScore: 3 },
@@ -35,41 +37,96 @@ const data1 = [
   { recipePhotos:['/assets/img/recipe2.png'],isLiked:1,likes:20, title: 'BBB', introduction: 'BBBsimple decoration', timeDuration: '20', rateScore: 3 },
   { recipePhotos: ['/assets/img/recipe3.png'],isLiked:0,likes:100, title: 'CCC', introduction: 'CCCsimple decoration', timeDuration: '25', rateScore: 5 },
 ]
-const JanpneseFood = () => {
+const ChineseFood = () => {
+
+  // const [fillheart, setFillHeart] = useState(0); // }
+  // const FillHeart = () => {
+  //     if(fillheart==0){
+  //       setFillHeart(1)
+  //     }
+  //     if(fillheart==1){
+  //       setFillHeart(0)
+  //     }
+  // };
+
+
+
+
   const [recipelist,setData] = useState([])
   const token = localStorage.getItem('token');
   React.useEffect(()=>{ 
     getInfo(token,setData)
   },[])
-//   // console.log(data1[0])
+  // console.log(data1[0])
   
-  console.log('recipelist is  ',recipelist)
-  console.log('hhhhh', recipelist['0'])
+  // console.log('recipelist is  ',recipelist)
+  // console.log('hhhhh', recipelist[0])
+
+
+
   const like = (i)=>{
-      let d = [...recipelist];
-      if(d[i].isLiked){
-          d[i].isLiked = 0;
-          d[i].likes--;
-      }else{
-          d[i].isLiked = 1;
-          d[i].likes++;
-      }
+    let d = [...recipelist];
+    // console.log('xxxxxxxxxxx',d[0][0].likes)
+
+    var recipeId = d[0][i].recipeId
+    
+    if(d[0][i].isLiked){
+        d[0][i].isLiked = 0;
+        d[0][i].likes--;
+        console.log('recipe ID is :', d[0][i].recipeId)
+        
+        recipeId = d[0][i].recipeId
+
+        const payload = JSON.stringify({
+          recipeId:recipeId
+        });
+        const result = FetchFunc(`recipe/unlike`, 'POST', token, payload);
+        console.log(result)
+        result.then((data) => {
+          console.log('mypost unlike data is',data);
+
+          if (data.status === 200) {
+            console.log('post unLike success')
+          }
+        })
+
+    }else{
+        d[0][i].isLiked = 1;
+        d[0][i].likes++;
+        console.log('recipe ID is :', d[0][i].recipeId)
+        
+        recipeId = d[0][i].recipeId
+
+        const payload = JSON.stringify({
+          recipeId:recipeId
+        });
+        const result = FetchFunc(`recipe/like`, 'POST', token, payload);
+        console.log(result)
+        result.then((data) => {
+          console.log('mypost data is',data);
+          if (data.status === 200) {
+            console.log('post Like success')
+          }
+        })
+ 
+    }
+    
+    setData(d)
       
-      React.useEffect(setData(d), [])
   }
   
-
 
    return (
 
         
         <h1>
-          
-            <h2 className='subtitle'>Japanese Food Recipe</h2>
+            
+            <h2 className='subtitle'>Chinese Food Recipe</h2>
             <p style={ { textAlign: 'center',fontSize:20 } }>simple decorationsimple decorationsimple decoration</p>
-            <FoodList data={recipelist} like={like} />
+            {/* <FoodList data={recipelist} FillHeart={FillHeart} fillheart = {fillheart}/> */}
+            <FoodList data={ recipelist} like={like} />
         </h1>
     )
 }
 
-export default JanpneseFood
+export default ChineseFood
