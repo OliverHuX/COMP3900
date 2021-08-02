123df4ea1ce623224a364dbe274323c1d565a189
OliverHuX <permanent0810@gmail.com>
2021-08-02 18:16:17 +0800

    update

--- frontend/src/components/TextPopup.jsx
+++ frontend/src/components/TextPopup.jsx
@@ -5,6 +5,8 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 
 export function TextPopup ({ open = false, setOpen, title = 'Notification', msg, newButton, newButtonMsg, newButtonFun }) {
   const handleClose = () => setOpen(false);
@@ -14,6 +16,8 @@ export function TextPopup ({ open = false, setOpen, title = 'Notification', msg,
         <DialogTitle id="form-dialog-title">{title}</DialogTitle>
         <DialogContent>
           {msg}
+          {title == 'Congratulation!' ? <CheckCircleOutlineIcon style={{ float: 'right', bottom: '70%', width: '40px', height: '40px' }} />
+          : <ErrorOutlineIcon style={{ float: 'right', bottom: '70%', width: '40px', height: '40px' }} />}
         </DialogContent>
         <DialogActions>
           {newButton && <Button onClick={() => newButtonFun} color="primary">
